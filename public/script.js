9c2697e0a17bd74d0209127c252bea212b1a9781
Ryan Krenzke <ryan.krenzke@gmail.com>
2020-10-07 16:26:35 -0400

    rover api improvement variety

--- public/script.js
+++ public/script.js
@@ -1,7 +1,8 @@
 //APIs
     /*NASA API*/
 const baseImageUrl = 'https://images-api.nasa.gov';
-const marsRoverUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol='//need a variable to randomly generate sol day #
+const marsRoverUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/'
+// const marsRoverUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol='
 const nasaKey = 'm4yeuGBjITcuHoSRECivvXxzhSVNEb0JaeI6nabk';
     /*Le Systeme Soliare API*/
 const lssUrl = 'https://api.le-systeme-solaire.net/rest/bodies/';
@@ -29,6 +30,7 @@ const listOfMoons = document.getElementById('moons');
 const description = document.getElementById('description');
 const imageBody = document.getElementById('carousel-inner');
 const orbitBorder = document.getElementsByTagName('li');
+const newMarsSol = document.getElementById('newMarsSol');
 
 //EVENTLISTENERS
 sun.addEventListener('click', fetchResults);
@@ -40,9 +42,11 @@ jupiter.addEventListener('click', fetchResults);
 saturn.addEventListener('click', fetchResults);
 uranus.addEventListener('click', fetchResults);
 neptune.addEventListener('click', fetchResults);
+newMarsSol.addEventListener('click', fetchMars);
 
 //CONDITIONAL STYLING
 results.style.display = 'none';
+newMarsSol.style.display = 'none';
 
 
 //CLASSES
@@ -69,6 +73,68 @@ function fetchResults(e){
         .then(json => {console.log(json); createPlanet(json)});
 }
 
+function getRandomMarsSol(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomRover(){
+    let roverArray = ['curiosity', 'spirit', 'opportunity'];
+    let randomRover = roverArray[Math.floor(Math.random()*roverArray.length)];
+    return randomRover;
+}
+
+function fetchMars(){
+    let solNumber = getRandomMarsSol(100, 1500);
+    let roverName = getRandomRover();
+    url = marsRoverUrl + roverName + '/photos?sol=' + solNumber + '&api_key=' + nasaKey;
+
+    fetch(url)
+        .then(result => {return result.json()})
+        .then(json => {displayRover(json)});
+}
+
+function displayRover(json){
+    newMarsSol.style.display = '';
+    console.log(json);
+    while(imageBody.firstChild){
+        imageBody.removeChild(imageBody.firstChild)
+    }
+
+    //Iterate through the first 30 images returned and create a carousel card for each
+    let current = json.photos
+    let activeDiv = document.createElement('div');
+    let activeImg = document.createElement('img');
+    activeDiv.setAttribute('class', 'carousel-item active');
+    activeImg.setAttribute('class', 'd-block w-100');
+    activeImg.src = current[0].img_src;
+    activeDiv.appendChild(activeImg);
+    imageBody.appendChild(activeDiv);
+
+    for(let i = 1; i < 30; i++){
+        let carouselDiv = document.createElement('div');
+        carouselDiv.setAttribute('class', 'carousel-item');
+        let carouselImg = document.createElement('img');
+        carouselImg.setAttribute('class', 'd-block w-100');
+        let carouselCaption = document.createElement('div');
+        carouselCaption.setAttribute('class', 'carousel-caption d-none d-md-block');
+        let captionText = document.createElement('p');
+
+        let solDate = current[i].sol;
+
+        let cameraName = current[i].camera.full_name;
+        let roverName = current[i].rover.name;
+        let imageDate = current[i].earth_date;
+        captionText.innerText = `Taken on Sol ${solDate} (${imageDate}) from the ${cameraName} onboard the ${roverName} Rover`;
+        carouselImg.src = current[i].img_src;
+        carouselCaption.appendChild(captionText);
+        carouselDiv.appendChild(carouselImg);
+        carouselDiv.appendChild(carouselCaption);    
+        imageBody.appendChild(carouselDiv);
+        }
+}
+
 function createPlanet(json){
     if(json.moons === null){
         numberOfMoons = 0;
@@ -78,7 +144,11 @@ function createPlanet(json){
     let newPlanet = new Planet(json.englishName, json.mass.massExponent, json.perihelion, json.aphelion, numberOfMoons)
     let planetName = newPlanet.name;
     displayPlanet(newPlanet);
-    getImage(planetName); 
+    if(planetName == 'Mars'){
+        fetchMars();
+    }else{
+        getImage(planetName);
+    } 
 }
   
 function displayPlanet(newPlanet){
@@ -105,6 +175,7 @@ function displayPlanet(newPlanet){
             orbit[0].style.border = 'none';
         }
     results.style.display = '';
+    newMarsSol.style.display = 'none';
     
 }
 
